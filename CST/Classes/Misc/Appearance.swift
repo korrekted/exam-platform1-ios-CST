dc994249d6d0193d07d00968eab660bffe6ab20a
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-06-12 14:55:59 +0300

    copy

---
+++ CST/Classes/Misc/Appearance.swift
@@ -0,0 +1,21 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 253, green: 252, blue: 255)
+    
+    static let mainColor = UIColor(integralRed: 97, green: 76, blue: 212)
+    static let mainColorAlpha = UIColor(integralRed: 97, green: 76, blue: 212, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 255, green: 97, blue: 97)
+    
+    static let progress1Color = UIColor(integralRed: 97, green: 76, blue: 212)
+    static let progress2Color = UIColor(integralRed: 255, green: 97, blue: 97)
+    static let progress3Color = UIColor(integralRed: 49, green: 30, blue: 105)
+}
